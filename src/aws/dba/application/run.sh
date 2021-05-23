c7fe284a276d5fe7b3591968658d82c9f3aea718
EC2 Default User <ec2-user@ip-172-31-30-19.us-west-2.compute.internal>
2021-05-23 04:38:33 +0000

    create DBA rest server

---
+++ src/aws/dba/application/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export FLASK_APP=DBAapp.py
+
+# docker container is launched from /, so we need to cd to the relevant code.
+[[ "$(whoami)" == "root" ]] && cd /application
+
+if ! [ -e "$FLASK_APP" ]; then
+    echo "ERROR: '$FLASK_APP' does not exist"
+    exit 1
+fi
+
+if [[ "$(whoami)" != "root" ]]; then
+    export FLASK_ENV=development
+    python -m flask run
+else
+    python -m flask run -h 0.0.0.0 -p 81
+fi
